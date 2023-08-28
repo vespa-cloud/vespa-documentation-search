c815c6dd1d2b49872c5b91216c53a2744f4d4ea6
Jo Kristian Bergum <bergum@yahooinc.com>
2023-08-28 12:48:10 +0200

    Add question embeddings and implement server filtering

--- src/main/java/ai/vespa/cloud/docsearch/LLMSearcher.java
+++ src/main/java/ai/vespa/cloud/docsearch/LLMSearcher.java
@@ -3,22 +3,23 @@ package ai.vespa.cloud.docsearch;
 
 import com.yahoo.component.annotation.Inject;
 import com.yahoo.component.provider.ComponentRegistry;
-import com.yahoo.language.Language;
 import com.yahoo.language.Linguistics;
 import com.yahoo.language.process.Embedder;
-import com.yahoo.language.process.StemMode;
-import com.yahoo.language.process.Token;
-import com.yahoo.prelude.query.*;
-import com.yahoo.processing.request.CompoundName;
+import com.yahoo.prelude.query.Item;
+import com.yahoo.prelude.query.AndItem;
+import com.yahoo.prelude.query.OrItem;
+import com.yahoo.prelude.query.WordItem;
+import com.yahoo.prelude.query.NullItem;
+import com.yahoo.prelude.query.NearestNeighborItem;
+import com.yahoo.prelude.query.WeakAndItem;
+import com.yahoo.prelude.query.RankItem;
 import com.yahoo.search.Query;
 import com.yahoo.search.Result;
 import com.yahoo.search.Searcher;
-import com.yahoo.search.result.Hit;
+
 import com.yahoo.search.searchchain.Execution;
 import com.yahoo.tensor.Tensor;
 import com.yahoo.tensor.TensorType;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -55,19 +56,61 @@ public class LLMSearcher extends Searcher {
             if(!Question.isStopWord(term))
                 weakAndItem.addItem(new WordItem(term, true));
         }
-        NearestNeighborItem nn = new NearestNeighborItem("embedding", "q");
-        nn.setTargetNumHits(100);
+
+        NearestNeighborItem paragraphQuery = new NearestNeighborItem("embedding", "q");
+        paragraphQuery.setTargetNumHits(100);
+        query.getRanking().getFeatures().put("query(q)", embedding);
+
+        NearestNeighborItem questionQuery = new NearestNeighborItem("question_embedding", "q");
+        questionQuery.setTargetNumHits(100);
         query.getRanking().getFeatures().put("query(q)", embedding);
 
         OrItem hybrid = new OrItem();
         hybrid.addItem(weakAndItem);
-        hybrid.addItem(nn);
+        hybrid.addItem(paragraphQuery);
+        hybrid.addItem(questionQuery);
 
         WordItem exact = new WordItem(queryStr,"questions_exact",true);
         RankItem rankItem = new RankItem();
-        rankItem.addItem(hybrid);
+
+        Item filter = filter(query);
+        if(!(filter instanceof NullItem)) {
+            AndItem and = new AndItem();
+            and.addItem(filter);
+            and.addItem(hybrid);
+            rankItem.addItem(and);
+        } else {
+            rankItem.addItem(hybrid);
+        }
         rankItem.addItem(exact);
         query.getModel().getQueryTree().setRoot(rankItem);
+    }
+
+    public static Item filter(Query query) {
+        String filter = query.properties().getString("filters");
+        if (filter == null || filter.isEmpty())
+            return new NullItem();
+        filter = filter.replace("+","");
+        String[] terms = filter.split(" ");
+
+        query.trace("terms " + List.of(terms),3);
+        if(terms.length == 0)
+            return new NullItem();
+
+        OrItem or = new OrItem();
+        for(String t: terms) {
+            String[] indexFilter = t.split(":");
+            if(indexFilter.length != 2)
+                continue;
+            String field = indexFilter[0];
+            String value = indexFilter[1];
+            WordItem w = new WordItem(value,field, true);
+            or.addItem(w);
+        }
+        query.trace("Query tree filter: " + or, 3);
+        if (or.getItemCount() == 0)
+            return new NullItem();
+        else return or;
     }
 
 }
