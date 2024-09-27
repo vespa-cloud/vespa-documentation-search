dfd3fa7c68c16484d7ed5550f67ba1fad879ef68
Johan Solbakken <johsol@vespa.ai>
2024-09-27 13:50:50 +0200

    Adds doc_id field to thread query profile

--- src/main/java/ai/vespa/cloud/docsearch/ThreadSearcher.java
+++ src/main/java/ai/vespa/cloud/docsearch/ThreadSearcher.java
@@ -35,12 +35,17 @@ public class ThreadSearcher extends Searcher {
 
         Result newResult = new Result(query);
         for (Hit group : threadIdGroupList) {
-            HitGroup hitGroup = new HitGroup(getThreadId((Group)group), group.getRelevance());
+            var threadId = getThreadId((Group)group);
+
+            HitGroup hitGroup = new HitGroup(threadId, group.getRelevance());
             for (Hit hitList : (HitGroup) group) {
                 for (Hit hit : (HitList) hitList) {
                     hitGroup.add(hit);
                 }
             }
+
+            hitGroup.setField("doc_id", threadId);
+
             newResult.hits().add(hitGroup);
         }
         return newResult;
